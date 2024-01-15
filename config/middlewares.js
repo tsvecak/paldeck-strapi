d84fa668d1f7f8646513ab999ab1a8320c25e570
tsvecak <tsvecak@gmail.com>
2024-01-15 22:14:01 +0000

    test 2

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,4 +1,24 @@
 module.exports = [
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
--- src/api/pal/middlewares/pals-list.js
+++ src/api/pal/middlewares/pals-list.js
@@ -7,19 +7,7 @@
 module.exports = (config, { strapi }) => {
   // Add your own logic here.
   return async (ctx, next) => {
-    ctx.query.populate = {
-      cover: {
-        fields: ['name', 'alternativeText', 'caption', 'url'],
-      },
-      elements: {
-        fields: ['name', 'color'],
-        populate: {
-          icon: {
-            fields: ['name', 'alternativeText', 'caption', 'url'],
-          }
-        }
-      },
-    }
+    ctx.query.populate = '*'
 
     await next();
   };
