10dae7c72bfe8e52efce0e4c0eee1386930e789b
tsvecak <tsvecak@gmail.com>
2024-01-15 22:46:45 +0000

    Correct filters

--- src/api/pal/middlewares/pals-list.js
+++ src/api/pal/middlewares/pals-list.js
@@ -7,7 +7,19 @@
 module.exports = (config, { strapi }) => {
   // Add your own logic here.
   return async (ctx, next) => {
-    ctx.query.populate = '*'
+    ctx.query.populate = {
+      cover: {
+        fields: ['name', 'alternativeText', 'caption', 'url'],
+      },
+      elements: {
+        fields: ['name', 'color'],
+        populate: {
+          icon: {
+            fields: ['name', 'alternativeText', 'caption', 'url'],
+          }
+        }
+      },
+    }
 
     await next();
   };
