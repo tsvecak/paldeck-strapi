40208fe8e3ac48835b900957ac9fc97e79c22de2
tsvecak <tsvecak@gmail.com>
2024-01-18 01:04:57 +0000

    Sort pals by string/number

--- src/api/pal/controllers/pal.js
+++ src/api/pal/controllers/pal.js
@@ -6,4 +6,16 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::pal.pal');
+module.exports = createCoreController('api::pal.pal',
+  () => ({
+    async find(ctx) {
+      ctx.query = { ...ctx.query, locale: "en" };
+      const result = await super.find(ctx);
+
+      console.log(result);
+      result.data.sort((a,b) => ("" + a.attributes.number).localeCompare(b.attributes.number, undefined, {numeric: true})); 
+
+      return result.data;
+    },
+  })
+);
