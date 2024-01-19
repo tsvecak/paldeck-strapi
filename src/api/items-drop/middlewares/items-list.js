c99949d7a240533249d7c20112f04c19a4917445
tsvecak <tsvecak@gmail.com>
2024-01-19 17:08:14 +0000

    Extending item drops API

---
+++ src/api/items-drop/middlewares/items-list.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * items-list controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = (config, { strapi }) => {
+  // Add your own logic here.
+  return async (ctx, next) => {
+    ctx.query.populate = {
+      pals: {
+        fields: ['name', 'slug', 'number'],
+        populate: {
+          elements: {
+            fields: ['name','slug', 'color'],
+            populate: {
+              icon: {
+                fields: ['name', 'alternativeText', 'caption', 'url'],
+              }
+            }
+          },
+        }
+      },
+    }
+
+    await next();
+  };
+};
--- src/api/items-drop/routes/items-drop.js
+++ src/api/items-drop/routes/items-drop.js
@@ -6,4 +6,10 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::items-drop.items-drop');
+module.exports = createCoreRouter('api::items-drop.items-drop', {
+  config: {
+    find: {
+      middlewares: ['api::items-drop.items-list'],
+    },
+  },
+ });
