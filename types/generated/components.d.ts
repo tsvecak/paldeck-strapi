26cb35c807c5e0532f51613cd6d36e09ded403ab
tsvecak <tsvecak@gmail.com>
2024-01-25 02:28:41 +0000

    Adding tech tree fields

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,21 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ItemsItemsRequired extends Schema.Component {
+  collectionName: 'components_items_items_requireds';
+  info: {
+    displayName: 'itemsRequired';
+    icon: 'bulletList';
+  };
+  attributes: {
+    requiredAmount: Attribute.Integer & Attribute.Required;
+    item: Attribute.Relation<
+      'items.items-required',
+      'oneToOne',
+      'api::items-drop.items-drop'
+    >;
+  };
+}
+
 export interface PalsStats extends Schema.Component {
   collectionName: 'components_pals_stats';
   info: {
@@ -16,6 +32,7 @@ export interface PalsStats extends Schema.Component {
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'items.items-required': ItemsItemsRequired;
       'pals.stats': PalsStats;
     }
   }
